91f2a741a1080a7b496c95fb5ce63f20ef420d77
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-11-26 14:32:30 +0530

    Update Login.jsx
    add_field

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,23 @@
-const Login=()=>{
-    return(
+const Login = () => {
+    return (
         <>
-        
+            <h2>Welcome to Login</h2>
+            <form>
+                <div>
+                    <div>Username</div>
+                    <div>
+                        <input type="text" placeholder="Enter your Username" />
+                    </div>                </div>
+                <div>
+                    <div>Password</div>
+                    <div>
+                        <input type="password" placeholder="Enter your Password" />
+                    </div>
+                </div>
+                <div>
+                    <button type="button">Login</button>
+                </div>
+            </form>
         </>
     )
 }
