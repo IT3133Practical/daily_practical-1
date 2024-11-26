a91377a12be0438d5e92b3e35b776f56fde7fbaf
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-11-26 14:32:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import './App.css';
 import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
 import AboutUs from './pages/AboutUs';
+import Login from './pages/auth/Login';
+import SignUp from './pages/auth/SignUp';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        Hello!!
         <nav>
           <ul>
             <Link to={"/"}><li>Home</li></Link>
             <Link to={"/contactus"}><li>ContactUs</li></Link>
             <Link to={"/aboutus"}><li>AboutUs</li></Link>
+            <Link to={"/login"}><li>Login</li></Link>
+            <Link to={"/signup"}><li>SignUp</li></Link>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
